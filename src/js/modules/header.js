e531a7fbec6d8a7b996aedd18ef19cb8ced4c7d9
Rz <243566431@qq.com>
2022-07-27 22:07:28 +0800

    修改搜索框的按钮及登录注册界面的手机端显示问题

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,23 +1,21 @@
-function searchContentWrap(){
+function searchContentWrap(display){
     const search_content_wrap = document.querySelector('#search_content_wrap')
-    const display = search_content_wrap.style.display
-    
-    if(display !== 'block') {
-        search_content_wrap.style.display  = 'block'
-    } else {
-        search_content_wrap.style.display  = 'none'
-    }
+    search_content_wrap.style.display  = display
 }
 
 module.exports = function(){
     const search_btn = document.querySelector('#search_btn')
     const search_btn_in_show = document.querySelector('#search_btn_in_show')
+    const close_btn_in_show = document.querySelector('#close_btn_in_show')
     const  search_input = document.querySelector('#search_input')
     search_btn && search_btn.addEventListener('click', function(){
-        searchContentWrap()
+        searchContentWrap('block')
     }, false)
     search_btn_in_show && search_btn_in_show.addEventListener('click', function(){
-        searchContentWrap()
+        searchContentWrap('block')
+    }, false)
+    close_btn_in_show && close_btn_in_show.addEventListener('click', function(){
+        searchContentWrap('none')
     }, false)
     search_input && search_input.addEventListener('keyup', function(e){
         if(e.keyCode === 27) {
